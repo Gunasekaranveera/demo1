6866c365da0d970f7e814a3a6069119eb478ff24
Gunasekaranveera <138692022+Gunasekaranveera@users.noreply.github.com>
2023-07-05 22:46:32 +0530

    Update sample4th commit.py
    char len check 25

--- sample.py
+++ sample.py
@@ -1,11 +1,4 @@
-def test_test1():
-    st="welcome to python"
-    assert len(st)==18
+def test_test4():
+    st="welcome to python tutorial"
+    assert len(st)==25
 
-def test_test2():
-    st1="welcome"
-    assert st1.upper()=="WELCOME"
-
-def test_test3():
-    st3="GOOD MORNING"
-    assert st3.lower()=="good night"
